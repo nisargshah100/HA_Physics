f1ff2868bb775a49c571e5574baf601deafd4e93
Edward Weng <edweng@gmail.com>
2012-06-20 14:26:27 -0400

    First commit

---
+++ newton/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newton::Application.config.secret_token = '9559b2e14b0febc3aa972fca285376868c5ec69af684a9685e5cfa99c6836b57e4c5e779f431af2e7b7c9deae86493fbf5627c1b7a0e5a21e485423a65d01c4f'
