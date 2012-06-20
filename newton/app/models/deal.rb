0ec1f4d8d8a7b092d635ad56a540057e2a27e18e
Nisarg Shah <nisargshah100@gmail.com>
2012-06-20 15:23:39 -0400

    livingsocial fetch

--- newton/app/models/deal.rb
+++ newton/app/models/deal.rb
@@ -1,3 +1,6 @@
 class Deal < ActiveRecord::Base
   # attr_accessible :title, :body
+  attr_accessible :livingsocial_id, :groupon_id, :date_added, :end_date, 
+                  :price_cents, :value_cents, :title, :subtitle, :affiliate_url, 
+                  :original_url, :image_url, :source
 end
