62cc867eb9f9641d054dada2112be2f0ed3f0aa0
Edward Weng <edweng@gmail.com>
2012-07-03 02:24:07 -0400

    Adding lazy susan index

---
+++ feynman/db/migrate/20120703062343_lazy_susan_for_authentications.rb
@@ -0,0 +1,25 @@
+class LazySusanForAuthentications < ActiveRecord::Migration
+  def change
+    add_index :authentications, :id
+    add_index :authentications, :user_id
+    add_index :authentications, :last_status_id
+    add_index :authentications, :authentication_id
+    add_index :deals, :id
+    add_index :deals, :original_id
+    add_index :events, :id
+    add_index :events, :deal_id
+    add_index :events, :user_id
+    add_index :events, [:deal_id, :user_id]
+    add_index :events, [:user_id, :deal_id]
+    add_index :images, :id
+    add_index :images, :user_id
+    add_index :messages, :id
+    add_index :messages, :sender_id
+    add_index :messages, :recipient_id
+    add_index :messages, [:sender_id, :recipient_id]
+    add_index :messages, [:recipient_id, :sender_id]
+    add_index :user_details, :id
+    add_index :user_details, :user_id
+    add_index :users, :id
+  end
+end
